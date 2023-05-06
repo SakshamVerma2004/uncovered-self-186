ee06b29b5fe534445c97d21a3733201fb63a87c9
veerabhadra30 <vbsraaj@gmail.com>
2023-05-06 18:06:55 +0530

    getstarted complete

--- Admin_Page/admin.js
+++ Admin_Page/admin.js
@@ -12,8 +12,111 @@ let sunroof=document.getElementById("sunroof");
 let CruiseControl=document.getElementById("CruiseControl");
 let Camera=document.getElementById("Camera");
 let ac=document.getElementById("ac");
-let addCars=document.getElementById("add");
+let addCars=document.getElementById("addCarsBtn");
+let addBus=document.getElementById("addBusBtn");
 
 addCars.addEventListener("click",function(){
-    console.log("clicked")
+    console.log("Cars")
+});
+addBus.addEventListener("click",function(){
+    console.log("Bus")
+});
+
+//********************************* */
+let data=JSON.parse(localStorage.getItem("bookdata"))||[];
+
+let main=document.getElementById("dataContainer");
+
+let fetchCarsData=document.getElementById("carsData")
+fetchCarsData.addEventListener("click",function(){
+    fetchCardata();
 })
+
+function fetchCardata(){
+    fetch("https://carsapi-mauve.vercel.app/data")
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        //console.log(data);
+        appenddata(data);
+    })
+}
+
+
+function appenddata(data){
+    main.innerHTML="";
+    let cardlist=document.createElement("div");
+    cardlist.className="card-list";
+    main.append(cardlist);
+
+    data.forEach(function(item){
+        cardlist.append(createcard(item));
+    })
+}
+
+
+function createcard(item){
+    let card=document.createElement("div");
+    card.className="card";
+    card.setAttribute("data-id",item.id);
+
+    let cardimg=document.createElement("div");
+    cardimg.className="card-img";
+
+    let img=document.createElement("img");
+    img.className="car-image";
+    img.src=item.image;
+    img.setAttribute("alt","cars");
+
+    cardimg.append(img);
+
+    let cardbody=document.createElement("div");
+    cardbody.className="card-body";
+
+    let ratingimg=document.createElement("img");
+    ratingimg.src="https://th.bing.com/th/id/OIP.ZlCJ0TGNGLIMu8XwbrAzRwHaHa?w=190&h=190&c=7&r=0&o=5&dpr=1.3&pid=1.7";
+    ratingimg.className="logo-img";
+
+    let rating=document.createElement("p");
+    rating.className="rating";
+    rating.textContent=`Ratings - ${item.ratings} `;
+
+    let trips=document.createElement("p");
+    trips.className="trips";
+    trips.textContent=` (${item.trips} Trips)`;
+
+    cardbody.append(ratingimg,rating,trips);
+
+    let carname=document.createElement("div");
+    carname.className="car-name";
+
+    let car=document.createElement("h3");
+    car.textContent=item.name;
+
+    let extra=document.createElement("p");
+    extra.textContent=`${item.type} - ${item.emission} - ${item.seats} seats`;
+
+    carname.append(car,extra);
+
+    let price=document.createElement("div");
+    price.className="price";
+
+    let money=document.createElement("h2");
+    money.textContent=`Rs.${item.price}/day`;
+    money.className="money";
+
+    let editData=document.createElement("button");
+    editData.className="edit";
+    editData.textContent="EDIT...!";
+
+    editData.addEventListener("click",function(){
+        console.log(item.id)
+    });
+
+    price.append(money,editData);
+
+    card.append(cardimg,cardbody,carname,price);
+    return card;
+}
+
---
+++ GetStarted/getstarted.js
@@ -0,0 +1,31 @@
+const button = document.getElementById('enterDetailsbtn');
+const form = document.getElementById('myForm');
+
+button.addEventListener('click', function() {
+  form.style.display = 'grid';
+});
+
+window.addEventListener('click', function(event) {
+    event.preventDefault()
+    if (event.target == form) {
+      form.style.display = 'none';
+    }
+  });
+
+let detailsSubmitBtn=document.getElementById("detailsSubmitBtn");
+detailsSubmitBtn.addEventListener("click",function(){
+    let UserData=[]
+    let obj={
+        name:document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        mobile:document.getElementById("mobile").value,
+        fromdate:document.getElementById("fromdate").value,
+        from:document.getElementById("from").value,
+        Destination:document.getElementById("Destination").value,
+        tilldate:document.getElementById("tilldate").value
+    }
+    UserData.push(obj)
+    localStorage.setItem("UserData",JSON.stringify(UserData));
+    window.location.reload()
+})
+  
